3f0e7d7ff4e6407ae7d4df0323bcd2c93886db1e
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 07:57:11 -0500

    refactor: refine hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,7 +7,10 @@ const Hero = () => (
     <div className="hero__badge">
       <small className="p-text">Hey, I am</small>
       <h1 className="head-text">Faraji</h1>
-      <SocialMedia />
+      <div className="flex hero__badge-container">
+        <SocialMedia />
+        <span className="hero__role"></span>
+      </div>
     </div>
     <img className="hero__img" src={images.profile} alt="profile_dp" />
   </section>
