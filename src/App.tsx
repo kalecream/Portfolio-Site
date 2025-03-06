01fa5bca86a3cfaf8a4d72446aba7fb354842d38
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 07:07:02 -0500

    chore: swap Skills component in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/index.scss';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
-import { Skills } from './container';
+import Skills from './components/Skills/Skills';
 
 function App() {
 
